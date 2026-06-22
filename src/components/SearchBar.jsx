c76dfd4ab451ed6a3ff3aa39bb266b6e87354cb9
Priyanshu Kumar Yadav <pky237917@gmail.com>
2026-06-22 22:52:41 +0530

    fixed search button overflow issue on mobile

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -10,9 +10,9 @@ function SearchBar({ city, setCity, onSearch }) {
     }
   }
   return (
-    <div className="flex gap-4 mb-4">
-      <input className="flex-1 bg-gray-800 border border-gray-600 focus:outline-none
-      focus:ring-2 focus:ring-blue-500 rounded-lg px-4 py-2 text-white placeholder-gray-400"
+    <div className="flex gap-2 mb-4">
+      <input className="flex-1 bg-gray-800 border border-gray-600 focus:outline-none shrink-0
+      focus:ring-2 focus:ring-blue-500 rounded-lg px-3 md:px-4 py-2 text-white placeholder-gray-400"
       type="text" value={city} onChange={(e) => setCity(e.target.value)} 
       onKeyDown={handleKeyDown} ref={inputElement} placeholder="Enter city.." />
       <button className="bg-blue-600 rounded-lg px-4 py-2 text-white hover:bg-blue-700 cursor-pointer" onClick={onSearch} >Search</button>
