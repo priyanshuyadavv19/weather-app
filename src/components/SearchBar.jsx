115da726b9b7b7055ba44ecc7d4381fc45fe8ffb
Priyanshu Kumar Yadav <pky237917@gmail.com>
2026-06-22 22:14:27 +0530

    Fix one issue

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -10,7 +10,7 @@ function SearchBar({ city, setCity, onSearch }) {
     }
   }
   return (
-    <div className="flex gap-6 mb-4">
+    <div className="flex gap-4 mb-4">
       <input className="flex-1 bg-gray-800 border border-gray-600 focus:outline-none
       focus:ring-2 focus:ring-blue-500 rounded-lg px-4 py-2 text-white placeholder-gray-400"
       type="text" value={city} onChange={(e) => setCity(e.target.value)} 
